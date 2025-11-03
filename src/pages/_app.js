b06d0979fc0331549c7702cf0d9bdbf8f698e620
Hari Om Bhardwaj <hariombhardwaj038@gmail.com>
2025-11-03 14:42:03 +0530

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    function setNavbarHeight() {
+      const navbar = document.querySelector('nav'); // Select the nav element
+      if (navbar) {
+        document.documentElement.style.setProperty('--navbar-height', `${navbar.offsetHeight}px`);
+      }
+    }
+
+    // Set on load and resize
+    setNavbarHeight();
+    window.addEventListener('resize', setNavbarHeight);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('resize', setNavbarHeight);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
